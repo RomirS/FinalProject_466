466976c7f290e6e05432d0810cd132de64aa7ebd
sohomsd <sohomdash01@gmail.com>
2023-12-13 12:57:44 -0600

    relaxed neighbor joining and plot

--- nj_functions.py
+++ nj_functions.py
@@ -1,3 +1,5 @@
+import copy
+
 def min_S_value(D, u):
     min_S, min_i, min_j = float("inf"),-1,-1
     for k in D:
@@ -11,7 +13,7 @@ def min_S_value(D, u):
     return (min_i, min_j)
 
 def neighbor_join(D):
-    D = D.copy()
+    D = copy.deepcopy(D)
 
     T = {k:{} for k in D}
     r = len(D)
@@ -24,7 +26,7 @@ def neighbor_join(D):
       D[r] = {}
       for m in D:
         if m not in (i, j, r):
-          D[r][m] = D[m][r] = 0.5*(D[i][m] + D[j][m])
+          D[r][m] = D[m][r] = 0.5*(D[i][m] + D[j][m] - D[i][j])
 
       # Add new vertex r to T with edges to ci and cj
       T[r] = {}
@@ -43,4 +45,60 @@ def neighbor_join(D):
 
       r += 1
 
+    return T
+
+
+def relaxed_neighbor_join(D):
+    D = copy.deepcopy(D)
+
+    T = {k:{} for k in D}
+    r = len(D)
+    while len(D)>2:
+      n = len(D)
+      i, j, ui, uj = None, None, None, None
+      for a in D:
+        S_a = {D[a][k] - (sum(D[a].values()) + sum(D[k].values()) - 2*D[a][k]) / (n - 2) for k in D if k != a}
+        for b in D:
+          if a == b:
+            continue
+          S_b = {D[b][k] - (sum(D[b].values()) + sum(D[k].values()) - 2*D[b][k]) / (n - 2) for k in D if k != b}
+
+
+          ui = (sum(D[a].values()) - D[a][b]) / (n - 2)
+          uj = (sum(D[b].values()) - D[a][b]) / (n - 2)
+          S_ab = D[a][b] - ui - uj
+
+          if S_ab <= min(S_a) and S_ab <= min(S_b):
+            i, j = a, b
+            break
+
+        if i != None and j != None:
+          break
+            
+      T[r] = {}
+      T[i][r] = T[r][i] = 0.5*(D[i][j] + ui - uj)
+      T[j][r] = T[r][j] = 0.5*(D[i][j] + uj - ui)
+      
+      #Compute D(r, m) for every other cluster m and add to D
+      D[r] = {}
+      for m in D:
+        if m not in (i, j, r):
+          D[r][m] = D[m][r] = 0.5*(D[i][m] + D[j][m] - D[i][j])
+
+      # remove ci and cj from D
+      del D[i]
+      del D[j]
+
+      for m in D:
+        if i in D[m]:
+          del D[m][i]
+        if j in D[m]:
+          del D[m][j]
+
+      r += 1
+
+    i, j = D.keys()
+    T[r] = {}
+    T[i][r] = T[r][i] = T[j][r] = T[r][j] = 0.5*D[i][j]
+    
     return T
