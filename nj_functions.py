301cbc25ada4e6257a5abaadbd437ec92d555194
sohomsd <sohomdash01@gmail.com>
2023-12-15 00:38:54 -0600

    rapid neighbor joining

--- nj_functions.py
+++ nj_functions.py
@@ -1,4 +1,5 @@
-import copy
+from copy import deepcopy
+import numpy as np
 
 def min_S_value(D, u):
     min_S, min_i, min_j = float("inf"),-1,-1
@@ -13,7 +14,7 @@ def min_S_value(D, u):
     return (min_i, min_j)
 
 def neighbor_join(D):
-    D = copy.deepcopy(D)
+    D = deepcopy(D)
 
     T = {k:{} for k in D}
     r = len(D)
@@ -49,7 +50,7 @@ def neighbor_join(D):
 
 
 def relaxed_neighbor_join(D):
-    D = copy.deepcopy(D)
+    D = deepcopy(D)
 
     T = {k:{} for k in D}
     r = len(D)
@@ -101,4 +102,91 @@ def relaxed_neighbor_join(D):
     T[r] = {}
     T[i][r] = T[r][i] = T[j][r] = T[r][j] = 0.5*D[i][j]
     
+    return T
+
+
+def rapid_neighbor_join(D, idx2node):
+    # initialize data structures
+    D = deepcopy(D)
+    idx2node = deepcopy(idx2node)
+    I = {}
+    S = {}
+    for row in D:
+      I[row] = np.argsort(D[row])
+      S[row] = np.take_along_axis(D[row], I[row], axis=0)
+
+    merged_nodes = set()
+    T = {k: {} for k in idx2node}
+
+    m_idx = len(D)
+    node_count = len(D)
+
+    while node_count > 2:
+      u = {}
+      for row in D:
+        if row in merged_nodes:
+          continue
+
+        u[row] = 0
+        for col in range(len(D)):
+          if idx2node[col] not in merged_nodes:
+            u[row] += D[row][col] / (node_count - 2)
+
+      umax = max(u.values())
+      qmin, i, j = float("inf"), -1, -1
+
+      for r in range(len(S)):
+        r_node = idx2node[r]
+        if r_node in merged_nodes:
+          continue
+
+        for c in range(len(S[r_node])):
+          curr_j = I[r_node][c]
+          j_node = idx2node[curr_j]
+          if j_node in merged_nodes or r == curr_j:
+            continue
+          
+          curr_S = S[r_node][c]
+          if curr_S - u[r_node] - umax > qmin:
+            break
+          
+          curr_Q = D[r_node][curr_j] - u[r_node] - u[j_node]
+          if curr_Q < qmin:
+            qmin, i, j = curr_Q, r, curr_j
+
+      i_node = idx2node[i]
+      j_node = idx2node[j]
+
+      m = str(m_idx)
+      T[m] = {}
+      T[i_node][m] = T[m][i_node] = 0.5*(D[i_node][j] + u[i_node] - u[j_node])
+      T[j_node][m] = T[m][j_node] = 0.5*(D[i_node][j] + u[j_node] - u[i_node])
+
+
+      merged_nodes.update([i_node, j_node])
+      idx2node = np.append(idx2node, m)
+      D[m] = np.zeros(len(idx2node))
+
+      for r in range(len(D)):
+        r_node = idx2node[r]
+        if r_node not in merged_nodes and r_node != m:                  
+          D_xr = 0.5*(D[i_node][r] + D[j_node][r] - D[i_node][j])     # distance value for new node to other nodes
+          D[r_node] = np.append(D[r_node], D_xr)                      # add new distance to end of each row
+          D[m][r] = D_xr                                    # update new row for D with new distance value
+            
+          insert_idx = np.searchsorted(S[r_node], D_xr)          # index for where to put new node distance in existing array
+          I[r_node] = np.insert(I[r_node], insert_idx, m_idx)             # insert index for new node into row for I
+          S[r_node] = np.insert(S[r_node], insert_idx, D_xr)          # insert new node distance into corresponding row for new S array
+
+      I[m] = np.argsort(D[m])
+      S[m] = np.take_along_axis(D[m], I[m], axis=0)
+
+      m_idx += 1
+      node_count -= 1
+
+    i, j = [k for k in D if k not in merged_nodes]
+    m = str(m_idx)
+    T[m] = {}
+    T[i][m] = T[m][i] = T[j][m] = T[m][j] = 0.5*D[i][int(j)]
+
     return T
